02966f6b46f1c9d8e92a375341af6ebbf9e15d5d
Daru31 <dalbit3189@naver.com>
2024-12-20 02:59:38 +0900

    first commit

--- autobill.py
+++
@@ -1,5 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="OlFtojxybMFMxBTfdLEF")
-project = rf.workspace("education-oqzdo").project("snack-eej94")
-version = project.version(1)
-dataset = version.download("yolov9") 
